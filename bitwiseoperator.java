da76f6c557e2f96f713f13d40e8ef85c25445da0
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-07-20 16:31:47 +0530

    some question is added

--- bitwiseoperator.java
+++ bitwiseoperator.java
@@ -31,8 +31,79 @@ public class bitwiseoperator {
         System.out.println();
         }
 
+        //question 
+        //Q1. check odd and even no
+        //for example -> 1--00000001 , 2--00000010
+        //for this example we can se that for odd RMB is 1 and for even RMB is 0
+
+        int c=10;
+        if ((c&1)==0){
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
 
 
-   }
+        //Q2.check the power of 2
+        //in power of 2 there will be 1 set bit for ex-->16= 00010000
+
+        int d=16;
+        if ((n&(n-1))==0) {
+           System.out.println("power of 2");
+            
+        } else {
+            System.out.println("not a power of 2");
+
+            
+        }
+
+
+       //Q3. swap two number  using XOR
+        int e= 10;
+        int f = 20;
+
+        System.out.println("Before Swapping:");
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+
+        e = e ^ f;
+        f = e ^ f;
+        e = e ^ f;
+
+        System.out.println("After Swapping:");
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+
+
+        // find unique element all other appear twice--xor method
+        //a ^ a = 0
+        //a ^ 0 = a
+
+         int[] arr = {2, 4, 5, 2, 4, 5, 7};
+
+        int ans = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            ans = ans ^ arr[i];
+        }
+
+        System.out.println("Unique element is: " + ans);
+
+
+        // for removing the last set bit
+        int g=10;
+        System.out.println((g&(g-1)));
     
+    // get last set bit
+
+     int h = 10;
+
+        int lastSetBit = h & -h;
+
+        System.out.println("Last set bit = " + lastSetBit);
+    }
 }
+
+    
+
