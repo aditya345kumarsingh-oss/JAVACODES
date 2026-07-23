d95f00dbe4d91c3a380c3d785c16e77f3cc2ca40
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-07-23 17:59:14 +0530

    program added

---
+++ arrayque3.java
@@ -0,0 +1,38 @@
+public class arrayque3 {
+
+    //Q1 soart an array of 0s and 1s
+    static int[] sort(int[] arr) {
+    int n = arr.length;
+    int i = 0;
+    int j = n - 1;
+
+    while (i < j) {
+
+        if (arr[i] == 1 && arr[j] == 0) {
+            arr[i] = 0;
+            arr[j] = 1;
+            i++;
+            j--;
+        }
+        else if (arr[i] == 0) {
+            i++;
+        }
+        else if (arr[j] == 1) {
+            j--;
+        }
+    }
+
+    return arr;
+}
+    public static void main(String[] args) {
+        int arr[] = {1, 0, 1, 0, 1, 0};
+          sort(arr);
+
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        
+    }
+    
+}
