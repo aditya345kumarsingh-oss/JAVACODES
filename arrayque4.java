02869a262a73622ca8daa3b93397aa8b8e833cd6
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-07-24 21:26:40 +0530

    program added

--- arrayque4.java
+++ arrayque4.java
@@ -42,7 +42,29 @@ static void threesum(int[] brr, int targett){
         }
     }
 
+    // Q3. Remove duplicates from sorted array
+    static int remove(int crr[]){
+        int i=0;
+        int j=1;
+        int c=crr.length;
+        while(j<c){
+            if(crr [i]== crr[j]){
+                j++;
+
+            }
+            else{
+                i++;
+                crr [i]=crr[j];
+                j++;
+
+            }
+
+        }
+        return i+1;
+    }
+
     public static void main(String[] args) {
+        //Q1 part 
         int arr[]={2,3,4,5,6,};
         int target = 9;
 
@@ -54,10 +76,25 @@ static void threesum(int[] brr, int targett){
             System.out.println("Indices: " + result[0] + " " + result[1]);
             System.out.println("Values: " + arr[result[0]] + " " + arr[result[1]]);
         }
+        //Q2 part
 
   int[] brr = {2, 3, 4, 5, 6};
         int targett= 12;
 
         threesum(brr, targett);
+
+    //Q3 part
+
+        int[] crr = {1,2,2,2,3,3,4,4,5};
+
+        int length = remove(crr);
+
+        System.out.println("Length = " + length);
+
+        System.out.print("Array: ");
+
+        for (int i = 0; i < length; i++) {
+            System.out.print(crr[i] + " ");
+        }
     }
 }
