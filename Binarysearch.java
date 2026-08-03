b6d7d1354a7f0aa4b73ea6998fb3ac7368da5d8e
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-08-03 14:05:44 +0530

    binary search

---
+++ Binarysearch.java
@@ -0,0 +1,38 @@
+public class Binarysearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+       
+
+        while (left <= right) {
+            int mid = (left + right ) / 2;
+            
+
+            // Check if the target is present at mid
+            if (arr[mid] == target) {
+                return mid; // Target found
+            }
+
+            // If target is greater, ignore the left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else { // If target is smaller, ignore the right half
+                right = mid - 1;
+            }
+        }
+
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 10, 40};
+        int target = 10;
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not present");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+    
+}
