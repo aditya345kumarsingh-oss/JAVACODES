9593d8be7bbf32674f55ea3d7e5124a09ed033d1
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-08-04 15:01:17 +0530

    occurance

--- Bound.java
+++ Bound.java
@@ -1,12 +1,12 @@
 public class Bound {
-    //Lower bound &  Upper bound
+    //Lower bound 
 
     static int getLowerBound(int[] arr, int target) {
 
         int n = arr.length;
         int s = 0;
         int e = n - 1;
-        int ans = -1;
+        int ans = n;
 
         while (s <= e) {
 
@@ -26,17 +26,55 @@ public class Bound {
         return ans;
     }
 
-    
+    // Upper bound
+
+    static int getUpperBound(int[] arr, int target) {
+
+        int n = arr.length;
+        int s = 0;
+        int e = n - 1;
+        int ans = n;
+
+        while (s <= e) {
+
+            int mid = s + (e - s) / 2;
+
+            if (arr[mid] <= target) {
+
+                    
+                s = mid + 1;    // Search in the right half
+
+            } else {
+                ans = mid;    // Store the possible answer
+
+                e = mid - 1;    // Search in the left half
+            }
+        }
+
+        return ans;
+    }
+    // Count Occurrences
+    static int countOccurrence(int[] arr, int target) {
+        return getUpperBound(arr, target) - getLowerBound(arr, target);
+    }
 
     public static void main(String[] args) {
 
-        int[] arr = {10, 20, 30, 30, 30, 40, 45};
-       // int target = 30;
-         int target = 35;
+        int[] arr = {10, 20, 30, 30,40, 45 ,46,70};
+         int targets = 30;
+         int target = 25;
 
 
-        int result = getLowerBound(arr, target);
+         int result = getLowerBound(arr, targets);
+         int results = getUpperBound(arr, target);
 
-        System.out.println("Lower Bound Index = " + result);
-    }
+        System.out.println("Lower Bound Index = " + result);    
+        System.out.println("Upper Bound Index = " + results);
+        int count = countOccurrence(arr, target);
+        int counts = countOccurrence(arr, targets);
+
+        
+        System.out.println("Count of " + target + " = " + count);
+        System.out.println("Count of " + targets + " = " + counts);
 }
+    }
