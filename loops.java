8813ad85a6aae9271937d7446e612bf10b0a6e1e
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-08-06 21:28:36 +0530

    code renew

--- loops.java
+++ loops.java
@@ -1,83 +1,104 @@
-// public class loops {
-//     public static void main(String args[])
-//     {
-        // for  LOOP
-        // EXAMPLE 1
+public class loops {
+    public static void main(String[] args) {
 
-        // for (int i = 1; i <= 5; i++) {
-        //     System.out.println("The value of i is: " + i);
-        // }
-        //EXAMPLE 2
+        // =========================
+        // FOR LOOP - EXAMPLE 1
+        // =========================
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("The value of i is: " + i);
+        }
 
-        // for ( int i=1; i<=10; i+=2)
-        // {
-        //     System.out.println("The value of i is: " + i);
-        // }
+        System.out.println();
 
-        // nested for loop
-        // EXAMPLE 1
-        // for (int i = 1; i <= 3; i++) { //rows
-        //     for (int j = 1; j <= 3; j++) {//columns
-        //         System.out.print("*");
-        //     }
-        //     System.out.println(); // Move to the next line after each row
-        // }
-        //EXAMPLE 2
-        // for (int i=1; i<=5; i++)
-        // {
-        //     for (int j=1; j<=3; j++)
-        //     {
-        //         System.out.println("i= " + i + " , j = " + j );
-        //     }
-        // }
+        // =========================
+        // FOR LOOP - EXAMPLE 2
+        // =========================
+        for (int i = 1; i <= 10; i += 2) {
+            System.out.println("The value of i is: " + i);
+        }
 
-        // break and continue statement
-        // EXAMPLE 1
-        //  for (int i = 1; i <= 10; i++) {
-        //     if (i == 5) {
-        //          break; 
-        //     }
-        //     System.out.println("The value of i is: " + i);
-        // }
-        // for (int i = 1; i <= 10; i++) {
-        //     if (i == 5) {
-        //         continue; 
-        //     }
-        //     System.out.println("The value of i is: " + i);
-        // } 
+        System.out.println();
 
-        // while loop
-        // EXAMPLE 1
-        // int i =1 ;
-        // while (i <= 5) {
-        //     System.out.println("The value of i is: " + i);
-        //     i++;
-        // }
+        // =========================
+        // NESTED FOR LOOP - EXAMPLE 1
+        // =========================
+        for (int i = 1; i <= 3; i++) { // Rows
+            for (int j = 1; j <= 3; j++) { // Columns
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-        // nested while loop
-        // EXAMPLE 1 
-        // int i =1;
-        // while (i <= 2) { // Outer loop for rows
-        //     int j = 1;
-        //     while (j <= 3) { // Inner loop for columns
-        //         System.out.println("i= " + i + " , j = " + j);
-        //         j++;
-        //     }
-            
-        //     i++;
-        // }
+        System.out.println();
 
-        // do while loop
-        // EXAMPLE 1
-        // int i=1 ;
-        // do {
-        //     System.out.println("The value of i is: " + i);
-        //     i++;
-        // } while (i <= 5);
+        // =========================
+        // NESTED FOR LOOP - EXAMPLE 2
+        // =========================
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
 
+        System.out.println();
 
+        // =========================
+        // BREAK STATEMENT
+        // =========================
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println("The value of i is: " + i);
+        }
 
+        System.out.println();
 
+        // =========================
+        // CONTINUE STATEMENT
+        // =========================
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.println("The value of i is: " + i);
+        }
 
-    
-// }}
+        System.out.println();
+
+        // =========================
+        // WHILE LOOP - EXAMPLE 1
+        // =========================
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("The value of i is: " + i);
+            i++;
+        }
+
+        System.out.println();
+
+        // =========================
+        // NESTED WHILE LOOP
+        // =========================
+        i = 1;
+        while (i <= 2) {
+            int j = 1;
+            while (j <= 3) {
+                System.out.println("i = " + i + ", j = " + j);
+                j++;
+            }
+            i++;
+        }
+
+        System.out.println();
+
+        // =========================
+        // DO-WHILE LOOP
+        // =========================
+        i = 1;
+        do {
+            System.out.println("The value of i is: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
