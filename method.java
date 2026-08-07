51a2af8d7ace5989732c2b6fcf85934ba91ed4b0
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-08-07 19:44:11 +0530

    code change

--- method.java
+++ method.java
@@ -1,70 +1,76 @@
-// public class method {
-//    static void table(){
-    //     for (int i=1; i<=10; i++){
-    //         System.out.println(5*i);
-    //     }
-    // }
-    // public static void main(String[] args) {
-    //    System.out.println("aditya");
-    //    table();
-    //    System.out.println("kumar");
+public class method {
 
-//     }
-    
-// }
-// }
+    // =========================
+    // Simple Method
+    // =========================
+    static void table() {
+        System.out.println("Table of 5:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
+    }
 
+    // =========================
+    // Parameterized Method
+    // =========================
+    static void sum(int x, int y) {
+        System.out.println("Sum = " + (x + y));
+    }
 
-//parameterized method
-// static void sum(int x, int y){
-            
-//             System.out.println("Sum: " + (x+y));
-//         }
-//         static void main(String[] args) {
-//             sum(5, 10);
-//         }}
+    // =========================
+    // Non-Void Method
+    // =========================
+    static int add(int a, int b) {
+        return a + b;
+    }
 
-// non void method
-//   static int add(int a, int b){
-//     int sum = a+b;
-//     return sum;
-// }
-// static void main(String[] args) {
-//     int addition = add (5, 10);
-//     System.out.println("Sum: " + addition);
-    
-// }
-// }
+    // =========================
+    // Method Overloading
+    // =========================
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
 
-//method overloading
+    // =========================
+    // Call by Value
+    // =========================
+    static void change(int a) {
+        System.out.println("Value inside method before change: " + a);
+        a = a * 10;
+        System.out.println("Value inside method after change: " + a);
+    }
 
-//  static int add(int a, int b){
-//     int sum = a+b;
-//     return sum;
-// }
-//  static int add(int a, int b ,int c){
-//     int sum = a+b+c;
-//     return sum;
-// }
+    // =========================
+    // Main Method
+    // =========================
+    public static void main(String[] args) {
 
-// static void main(String[] args) {
-//     int addition = add (5, 10);
-//     int addition2 = add (5, 10, 15);
-//     System.out.println("Sum: " + addition);
-//     System.out.println("Sum: " + addition2);
-// }
-// }
+        // Simple Method
+        table();
 
-// call by value 
-//  static void change(int a){
-//     System.out.println("Value of a inside method: " + a);
-//     a=a*10;
-//     System.out.println("Value of a inside method after multiplication: " + a);
-// }
-// static void main(String[] args) {
-//     int a = 5;
-//     System.out.println("Value of a before method call: " + a);
-//     change(a);
-//     System.out.println("Value of a after method call: " + a);
-// }
-// }
+        System.out.println();
+
+        // Parameterized Method
+        sum(5, 10);
+
+        System.out.println();
+
+        // Non-Void Method
+        int addition = add(5, 10);
+        System.out.println("Addition of 2 numbers = " + addition);
+
+        System.out.println();
+
+        // Method Overloading
+        int addition2 = add(5, 10, 15);
+        System.out.println("Addition of 3 numbers = " + addition2);
+
+        System.out.println();
+
+        // Call by Value
+        int a = 5;
+        System.out.println("Value before method call = " + a);
+        change(a);
+        System.out.println("Value after method call = " + a);
+    }
+}
