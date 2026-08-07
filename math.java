fe2bab0a330b0fa2f7a59a915cc3cc738a0f3bb4
Aditya Kumar Singh <aditya345kumarsingh@gmail.com>
2026-08-07 19:42:02 +0530

    code correect

--- math.java
+++ math.java
@@ -1,157 +1,136 @@
-//public class math {
-                 //printing the digits of a number
-//    static void printdigits(int num){
-//        while(num!=0){
-//            int digit =num%10;
-//            System .out.println(digit);
-//            num=num/10;
-//        }
-//    }
-                //   count digits of a number
-//    static int countdigits(int num){
-//        int count=0;
-//       while(num!=0){
-//            int digit =num%10;
-//            count++;
-//            num=num/10;
-//        }
-//        return count;
-//    }
-    //sum of a digits
-//    static int sumdigits(int num){
-//        int sum=0;
-//        while(num!=0){
-//            int digit =num%10;
-//           sum=sum+digit;
-//            num=num/10;
-//        }
-//        return sum;
-//    }
-
-    //reverse number
-//    static int reversedigits(int num){
-//       int rev=0;
-//       while(num!=0){
-//            int digit =num%10;
-//           rev=rev*10+digit;
-//            num=num/10;
-//        }
-//       return rev;
-//    }
-    // public static void main(){
-//        int num =53127;
-//        int rev= reversedigits(num);
-//        System.out.println(rev);
-//       int sum=sumdigits(num);
-//        System.out.println(sum);
-//        int ans=countdigits(num);
-//        System.out.println(ans);
-//        printdigits(num) ;
-//
-
-
-//public class math {
-//    //Palindrom number
-//    static int reversedigits(int num) {
-//        int rev = 0;
-//        while (num != 0) {
-//            int digit = num % 10;
-//            rev = rev * 10 + digit;
-//            num = num / 10;
-//        }
-//        return rev;
-//    }
-//
-//    static boolean ispalindrom(int num) {
-//        int originalNum = num;
-//        int reversedigits = reversedigits(num);
-//        if (originalNum == reversedigits) {
-//            System.out.println("palindrom");
-//            return true;
-//        } else {
-//            System.out.println("not a palindrom");
-//            return false;
-//        }
-//    }
-
-//    public static void main() {
-//        boolean ans = ispalindrom(1221);
-//        System.out.println(ans);
-//    }
-//}
-
-//prime number
 public class math {
-//    static boolean isPrime(int num) {
-//        for (int i = 2; i * i <= num; i++) {
-//            if (num % i == 0) {
-//                return false;
-//            }
-//        }
-//
-//        return true;
-//    }
-    // GCD NUMBER
-    static int getgcd(int a , int b){
-        while (b!=0){
-            int temp=b;
-            b=a%b;
-            a=temp;
-        }
-        int ans=a;
-        return ans;
-    }
-    // LCM NUMBER
-//    static int getlcm(int a, int b){
-//        int gcd= getgcd(a, b);
-//        int prod= a*b;
-//        int lcm = prod/ gcd;
-//        return lcm ;
-//    }
-    static  boolean isarmstron(int num){
-        int sum =0;
-        int org=num;
-        while (num!=0){
-            int digit=num%10;
-            int cube = digit*digit*digit;
-            sum= sum+cube;
-            num=num/10;
 
+    // Print digits of a number
+    static void printDigits(int num) {
+        while (num != 0) {
+            int digit = num % 10;
+            System.out.println(digit);
+            num = num / 10;
         }
-        if ( sum == org){
-            return true;
+    }
+
+    // Count digits of a number
+    static int countDigits(int num) {
+        int count = 0;
+        while (num != 0) {
+            count++;
+            num = num / 10;
         }
-        else {
-            return  false;
+        return count;
+    }
+
+    // Sum of digits
+    static int sumDigits(int num) {
+        int sum = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit;
+            num = num / 10;
+        }
+        return sum;
+    }
+
+    // Reverse a number
+    static int reverseDigits(int num) {
+        int rev = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+        }
+        return rev;
+    }
+
+    // Palindrome number
+    static boolean isPalindrome(int num) {
+        return num == reverseDigits(num);
+    }
+
+    // Prime number
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
         }
 
-    }
-    // perfect number
-    static boolean perfect(int num){
-        int sum=1;
-        for (int i=2; i*i<=num;i++){
-        if(num %i==0){
-            int first=i;
-            int second =num/i;
-            sum=sum+first+second;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
         }
-    }
-   if ( sum == num){
+
         return true;
     }
-        else {
-        return  false;
+
+    // GCD (Euclidean Algorithm)
+    static int getGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 
-}
+    // LCM
+    static int getLCM(int a, int b) {
+        int gcd = getGCD(a, b);
+        return (a * b) / gcd;
+    }
+
+    // Armstrong Number (3-digit)
+    static boolean isArmstrong(int num) {
+        int sum = 0;
+        int original = num;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        return sum == original;
+    }
+
+    // Perfect Number
+    static boolean perfect(int num) {
+
+        if (num <= 1) {
+            return false;
+        }
+
+        int sum = 1;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+
+                if (i != num / i) {
+                    sum += num / i;
+                }
+            }
+        }
+
+        return sum == num;
+    }
 
     public static void main(String[] args) {
-        System.out.println(perfect(6 ));
-//        System.out.println(isarmstron(153));
-//        System.out.println(getlcm(10 , 20));
-//        int num = 21;
-//        System.out.println(isPrime(num));
+
+        int num = 53127;
+
+        System.out.println("Digits:");
+        printDigits(num);
+
+        System.out.println("\nCount of Digits: " + countDigits(num));
+        System.out.println("Sum of Digits: " + sumDigits(num));
+        System.out.println("Reverse: " + reverseDigits(num));
+
+        System.out.println("\nPalindrome (1221): " + isPalindrome(1221));
+        System.out.println("Prime (21): " + isPrime(21));
+
+        System.out.println("\nGCD of 10 and 20: " + getGCD(10, 20));
+        System.out.println("LCM of 10 and 20: " + getLCM(10, 20));
+
+        System.out.println("\nArmstrong (153): " + isArmstrong(153));
+        System.out.println("Perfect Number (6): " + perfect(6));
     }
 }
-    
-
