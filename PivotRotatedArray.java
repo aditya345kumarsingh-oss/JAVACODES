8b08f993e0a3cb3da5eca645c5206aed8d61d59d
Aditya kumar singh <aditya345kumarsingh@gmail.com>
2026-08-12 20:40:49 +0530

    pivotrotated

---
+++ PivotRotatedArray.java
@@ -0,0 +1,52 @@
+public class PivotRotatedArray {
+
+    public static int findPivotIndex(int[] nums) {
+
+        int n = nums.length;
+        int s = 0;
+        int e = n - 1;
+        int ans = -1;
+
+        // No effective rotation
+        if (nums[s] < nums[e]) {
+            return -1;
+        }
+
+        // Binary search logic
+        while (s <= e) {
+
+            int mid = s + (e - s) / 2;
+
+            // mid is on the right sorted part
+            if (nums[mid] <= nums[n - 1]) {
+
+                ans = mid;
+
+                // Move left to find the first element
+                // of the right sorted part
+                e = mid - 1;
+
+            } else {
+
+                // mid is on the left sorted part
+                // Move right
+                s = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {7, 9, 1, 2, 3};
+
+        int pivot = findPivotIndex(nums);
+
+        System.out.println("Pivot Index: " + pivot);
+
+        if (pivot != -1) {
+            System.out.println("Pivot Element: " + nums[pivot]);
+        }
+    }
+}
