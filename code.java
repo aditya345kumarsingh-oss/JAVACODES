e9476e1676893f0bdcb736db7323d042c513dcfe
Aditya kumar singh <aditya345kumarsingh@gmail.com>
2026-08-13 21:09:02 +0530

    code modefied

--- code.java
+++ code.java
@@ -1,29 +1,39 @@
-// import java.util.Scanner;
-// public class code {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         System.out.print("Enter the limit: ");
-//         int limit = sc.nextInt();
-        //Print counting from 1 to n
-        // for (int i = 1; i <= limit; i++) {
-        //     System.out.println("The value of i is: " + i);
-        // }
+import java.util.Scanner;
 
-        //Print counting from n to 1
-        // for (int i = limit; i >= 1; i--) {
-        //     System.out.println("The value of i is: " + i);
-        // }
+public class code {
 
-        //Print the first 10 multiplication  of n
-        // for (int i =1;i<=10;i++){
-        //     System.out.println(limit*i);
-        // }
+    public static void main(String[] args) {
 
-        //print name n times
-        // for (int i = 1; i <= limit; i++) {
-        //     System.out.println("Aditya");
-        // }
+        Scanner sc = new Scanner(System.in);
 
-    
-    // }}
+        System.out.print("Enter the limit: ");
+        int limit = sc.nextInt();
 
+
+        // 1. Print counting from 1 to n
+        for (int i = 1; i <= limit; i++) {
+            System.out.println("The value of i is: " + i);
+        }
+
+
+        // 2. Print counting from n to 1
+        for (int i = limit; i >= 1; i--) {
+            System.out.println("The value of i is: " + i);
+        }
+
+
+        // 3. Print the first 10 multiples of n
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(limit * i);
+        }
+
+
+        // 4. Print name n times
+        for (int i = 1; i <= limit; i++) {
+            System.out.println("Aditya");
+        }
+
+
+        sc.close();
+    }
+}
