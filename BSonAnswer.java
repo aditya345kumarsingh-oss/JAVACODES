a5e1623ec9728f46cbb86317210203f15f4fe625
Aditya kumar singh <aditya345kumarsingh@gmail.com>
2026-08-17 23:09:43 +0530

    binary search for answer

--- BSonAnswer.java
+++ BSonAnswer.java
@@ -1,38 +1,91 @@
 public class BSonAnswer {
 
-    public int mysqrt(int x){
-        int s=1;
-        int e=x;
-        int ans =-1;
+    public double mysqrt(int x) {
 
-        if (x==0){
+        int s = 1;
+        int e = x;
+        double ans = -1;
+
+        // Special case
+        if (x == 0) {
             return 0;
         }
 
-        while (s<=e) {
-            int mid =s+(e-s)/2;
+        // =========================
+        // INTEGER PART
+        // BINARY SEARCH
+        // =========================
+        while (s <= e) {
 
-            if (mid ==x/mid){
-                return mid;
-               }
+            int mid = s + (e - s) / 2;
 
-            else if ( mid >x/mid){
-                e=mid-1; // move left
-            }
-            else{
+            if (mid == x / mid) {
                 ans = mid;
-                s=mid+1;    // move right
+                break;
             }
 
+            else if (mid > x / mid) {
+                e = mid - 1; // move left
+            }
+
+            else {
+                ans = mid;
+                s = mid + 1; // move right
+            }
+        }
+
+
+        // =========================
+        // DECIMAL PART
+        // =========================
+        double factor = 1;
+        int totalPrecision = 3;
+
+        for (int round = 1; round <= totalPrecision; round++) {
+
+            factor = factor / 10;
+
+            for (int i = 1; i <= 10; i++) {
+
+                double newAns = ans + factor;
+
+                if (newAns * newAns == x) {
+                    ans = newAns;
+                    break;
+                }
+
+                else if (newAns * newAns < x) {
+                    ans = newAns;
+                }
+
+                else {
+                    break;
+                }
+            }
+
+            System.out.println(
+                "This round answer is: " + ans
+            );
+        }
+
+        System.out.println("Final answer is: " + ans);
+
+        return ans;
     }
-    return ans;
-}
+
+
     public static void main(String[] args) {
-        int x = 458753; // Example input for square root calculation
+
+        int x = 458753;
+
         BSonAnswer obj = new BSonAnswer();
-        int result = obj.mysqrt(x);
-        System.out.println("The square root of " + x + " is: " + result);
-        
+
+        double result = obj.mysqrt(x);
+
+        System.out.printf(
+            "The square root of %d is: %.3f%n",
+            x,
+            result
+        );
     }
-    
 }
