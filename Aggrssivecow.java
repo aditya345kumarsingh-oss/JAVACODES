a471d6cf248211c111c3cf0ec4c56ede042890dc
Aditya kumar singh <aditya345kumarsingh@gmail.com>
2026-08-23 20:32:10 +0530

    agressive cow

--- Aggrssivecow.java
+++ Aggrssivecow.java
@@ -1,16 +1,34 @@
-public class Aggrssivecow {
-    static boolean ispossible (int [] stalls, int cow , int maxdistance){
-        int count = 1;
-        int lastpos=stalls[0];
-        for (int i=1;i<stalls.length;){
-            if (stalls[i]-lastpos>=maxdistance){
-                count++;
-                lastpos=stalls[i];
-                
-            }
+import java.util.Arrays;
 
+public class Aggrssivecow {
+
+    static boolean isPossible(int[] stalls, int cows, int minimumDistance) {
+        int placedCows = 1;
+        int lastPosition = stalls[0];
+
+        for (int i = 1; i < stalls.length; i++) {
+            if (stalls[i] - lastPosition >= minimumDistance) {
+                placedCows++;
+                lastPosition = stalls[i];
+
+                if (placedCows >= cows) {
+                    return true;
+                }
+            }
         }
-        return count>=cow;
+
+        return false;
     }
-    
-}
+
+    static int aggressiveCows(int[] stalls, int cows) {
+        if (stalls == null || stalls.length == 0 || cows <= 0 || cows > stalls.length) {
+            return -1;
+        }
+
+        Arrays.sort(stalls);
+
+        int start = 0;
+        int end = stalls[stalls.length - 1] - stalls[0];
+        int answer = 0;
+
+     
