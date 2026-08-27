27005f7453a288adeb486930858a25511034e6be
Aditya kumar singh <aditya345kumarsingh@gmail.com>
2026-08-27 23:35:18 +0530

    Sliding window

---
+++ SlidingWindow.java
@@ -0,0 +1,39 @@
+public class SlidingWindow {
+
+    static int maxSum(int[] arr, int k) {
+
+        // Sum of first window
+        int windowSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            windowSum = windowSum + arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        // Move the window
+        for (int i = k; i < arr.length; i++) {
+
+            windowSum = windowSum + arr[i] - arr[i - k];
+
+            if (windowSum > maxSum) {
+                maxSum = windowSum;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 1, 5, 1, 3, 2};
+
+        int k = 3;
+
+        int result = maxSum(arr, k);
+
+        System.out.println(
+            "Maximum sum of " + k + " elements = " + result
+        );
+    }
+}
