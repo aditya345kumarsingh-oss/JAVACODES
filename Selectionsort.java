b15ccbf8d76e95ac0bb659b848e5f5f8b7e4dc9e
Aditya kumar singh <aditya345kumarsingh@gmail.com>
2026-08-27 20:16:24 +0530

    Selection sort

--- Selectionsort.java
+++ Selectionsort.java
@@ -15,7 +15,7 @@ public class Selectionsort {
         }
     }
     public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, };
+        int[] arr = {64, 34, 25, 12,22, 11, 90};
         selectionSort(arr);
         System.out.println("Sorted array:");
         for (int i = 0; i < arr.length; i++) {
