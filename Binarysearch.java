fc466c2cc22b40b4d1dcc93f5803482a6dd2dbba
Aditya kumar singh <aditya345kumarsingh@gmail.com>
2026-08-29 23:52:19 +0530

    Binarysearch

--- Binarysearch.java
+++ Binarysearch.java
@@ -25,8 +25,8 @@ public class Binarysearch {
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 3, 4, 10, 40};
-        int target = 10;
+        int[] arr = {2, 3, 4, 10, 40, 50};
+        int target = 40;
         int result = binarySearch(arr, target);
         if (result == -1) {
             System.out.println("Element not present");
